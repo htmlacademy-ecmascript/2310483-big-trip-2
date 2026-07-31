dac30f50861685fe7bce9c49b1288e8b2ae8823e
khepri_kreator <makareev.dima@yandex.ru>
2026-07-31 21:34:33 +0800

    feat: default sort state

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -3,7 +3,13 @@ import AbstractView from '../framework/view/abstract-view.js';
 const createSortTemplate = (sortOptions) => `
     <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       ${sortOptions.map((sortOption) => `<div class="trip-sort__item  ${sortOption.class}">
-          <input id="${sortOption.id}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="${sortOption.value}">
+          <input id="${sortOption.id}"
+            class="trip-sort__input  visually-hidden"
+            type="radio"
+            name="trip-sort"
+            value="${sortOption.value}"
+            ${sortOption.value === 'sort-day' ? 'checked' : ''}
+          >
           <label class="trip-sort__btn" for="${sortOption.id}">${sortOption.name}</label>
         </div>`).join('')}
     </form>
